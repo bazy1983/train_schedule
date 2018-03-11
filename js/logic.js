3db5cdfb00ab29bd422b1165bbf7d69a532c76d8
Mohammed Hameed <mohammed.hameed@email.com>
2018-03-10 18:31:15 -0600

    added firebase config and basic inputs

---
+++ js/logic.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+      // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBbz8hVF_czUjOPCdILZ9PtivEm-dwtRTY",
+    authDomain: "trainscheduler-bf7ad.firebaseapp.com",
+    databaseURL: "https://trainscheduler-bf7ad.firebaseio.com",
+    projectId: "trainscheduler-bf7ad",
+    storageBucket: "",
+    messagingSenderId: "35492814730"
+  };
+  firebase.initializeApp(config);
+
+
+  var db = firebase.database();
+  
+})
